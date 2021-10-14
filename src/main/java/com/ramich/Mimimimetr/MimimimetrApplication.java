ff63b10309ca9c1c9752f05d7d5281beb281a0a7
Ramil Safuanov <ramilfin@mail.ru>
2021-10-14 22:04:37 +0300

    Initial commit

---
+++ src/main/java/com/ramich/Mimimimetr/MimimimetrApplication.java
@@ -0,0 +1,16 @@
+package com.ramich.Mimimimetr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimimimetrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimimimetrApplication.class, args);
+	}
+
+	/*String str = "src\\com\\javarush\\Main.java";
+	Path path = Path.of(str).toAbsolutePath();*/
+
+}
---
+++ src/test/java/com/ramich/Mimimimetr/MimimimetrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramich.Mimimimetr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimimimetrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
