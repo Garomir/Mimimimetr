48e30f92c6f2537c8281cba54e644219920c56bf
Ramil Safuanov <ramilfin@mail.ru>
2021-10-14 23:41:17 +0300

    .

--- src/main/java/com/ramich/Mimimimetr/controllers/CatController.java
+++ src/main/java/com/ramich/Mimimimetr/controllers/CatController.java
@@ -7,13 +7,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.nio.file.Path;
 import java.util.List;
 
 @Controller
 public class CatController {
 
+    //String str = "\\imgs\\";
+    Path path = Path.of("\\imgs\\").toAbsolutePath();
+
     private CatService catService;
 
+    //Инжектим CatService
     @Autowired
     public void setCatService(CatService catService) {
         this.catService = catService;
