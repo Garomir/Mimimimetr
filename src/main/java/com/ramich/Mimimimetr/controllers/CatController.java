fc6caee1712b883dedb2a81db3716106b6cf5c66
Ramil Safuanov <ramilfin@mail.ru>
2021-10-14 23:34:09 +0300

    добавлены home.html и candidates.html, где можно посмотреть список наших котиков

--- src/main/java/com/ramich/Mimimimetr/controllers/CatController.java
+++ src/main/java/com/ramich/Mimimimetr/controllers/CatController.java
@@ -4,6 +4,7 @@ import com.ramich.Mimimimetr.entities.Cat;
 import com.ramich.Mimimimetr.services.CatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
@@ -16,5 +17,17 @@ public class CatController {
     @Autowired
     public void setCatService(CatService catService) {
         this.catService = catService;
+    }
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/candidates")
+    public String candidates(Model model){
+        List<Cat> cats = catService.findAllCats();
+        model.addAttribute("cats", cats);
+        return "candidates";
     }
 }
