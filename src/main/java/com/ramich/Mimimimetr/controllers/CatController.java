f6566d405b8c07531480d26e995a35e4863aec9e
Ramil Safuanov <ramilfin@mail.ru>
2021-10-15 00:40:08 +0300

    добавлена top10.html

--- src/main/java/com/ramich/Mimimimetr/controllers/CatController.java
+++ src/main/java/com/ramich/Mimimimetr/controllers/CatController.java
@@ -8,7 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.nio.file.Path;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class CatController {
@@ -31,5 +33,18 @@ public class CatController {
         List<Cat> cats = catService.findAllCats();
         model.addAttribute("cats", cats);
         return "candidates";
+    }
+
+    @GetMapping("/top10")
+    public String top10(Model model){
+        List<Cat> cats = catService.findAllCats();
+
+        List<Cat> top10 = cats.stream()
+                .sorted(Comparator.comparingInt(Cat::getLikes).reversed())
+                .limit(10)
+                .collect(Collectors.toList());
+
+        model.addAttribute("top10", top10);
+        return "top10";
     }
 }
--- src/main/java/com/ramich/Mimimimetr/services/CatService.java
+++ src/main/java/com/ramich/Mimimimetr/services/CatService.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface CatService {
     List<Cat> findAllCats();
+    //List<Cat> findTop10Cats();
 }
